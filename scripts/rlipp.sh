0c7486003c0fda6c73bd7dd4e5b8f3cc0baeecb7
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2022-08-10 19:33:40 -0500

    add own setting

--- scripts/rlipp.sh
+++ scripts/rlipp.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-homedir=$1
-ontology="${homedir}/data/training_files_av/ontology.txt"
-gene2idfile="${homedir}/data/training_files_av/gene2ind.txt"
-cell2idfile="${homedir}/data/training_files_av/cell2ind.txt"
-cell_mutation="${homedir}/data/training_files_av/cell2mutation.txt"
-test="${homedir}/data/training_files_av/test.txt"
+homedir='../graph_neural_network_drug_response/notebook/DrugCell/data/'
+ontology="${homedir}drugcell_ont.txt"
+gene2idfile="${homedir}gene2ind.txt"
+cell2idfile="${homedir}cell2ind.txt"
+cell_mutation="${homedir}cell2mutation.txt"
+test="${homedir}test_rcell_over50_not_equal.txt"
 
-modeldir="${homedir}/models/CNV/model"
-
-predicted="${modeldir}/predict.txt"
+modeldir='/export/scratch/inoue019/'
+predicted="${modeldir}Result_157/drugcell.predict"
 output="${modeldir}/rlipp.out"
-hidden="${modeldir}/hidden"
+hidden="${modeldir}Hidden_157"
 
-cpu_count=$2
+cpu_count=30
 
-neurons=6
+neurons=5
 
-python -u ${homedir}/src/rlipp_helper.py -hidden $hidden -ontology $ontology -test $test  \
+/export/scratch/inoue019/envs/cluster/bin/python -u /home/inoue019/code/rlipp/src/rlipp_helper.py -hidden $hidden -ontology $ontology -test $test  \
 	-gene2idfile $gene2idfile -cell2idfile $cell2idfile -cell_mutation $cell_mutation -output $output \
 	-predicted $predicted -cpu_count $cpu_count -drug_count 0 -genotype_hiddens $neurons > "${modeldir}/rlipp.log"
